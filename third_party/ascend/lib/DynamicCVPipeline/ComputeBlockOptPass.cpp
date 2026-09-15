eb255da50584f411ad8d80d12122fe7df0028228
licf497 <1309837013@qq.com>
2026-09-15 22:27:19 +0800

    [ssbuffer](fix) support matmulRes used by mamtul's input and init (#2145)
    * [ssbuffer](fix) support matmulRes used by mamtul's input and init
    
    * [ssbuffer](fix) modify for review comment

--- third_party/ascend/lib/DynamicCVPipeline/ComputeBlockOptPass.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/ComputeBlockOptPass.cpp
@@ -81,6 +81,7 @@ void ComputeBlockOptPass::runOnOperation() {
   pm.addPass(createReorderOpsByBlockIdPass());
 
   pm.addPass(createMergeCubeBlockPass());
+  pm.addPass(createMergeInputInitSharedCubeBlockPass());
   pm.addPass(createReorderOpsByBlockIdPass());
   pm.addPass(createRelocateMemrefDeclPass());
 
@@ -118,6 +119,7 @@ void registerComputeBlockOptPasses() {
   registerPass(createMergeSmallBlockPass);
   registerPass(createSplitIfByBlockIdPass);
   registerPass(createMergeComputeBlockPass);
+  registerPass(createMergeInputInitSharedCubeBlockPass);
   registerPass(createMergeCubeBlockPass);
   registerPass(createRelocateMemrefDeclPass);
 }
